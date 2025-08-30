5126112e51c4a9b5c3f926f3a8438110dfa84166
FukaMiya <ninja8rrrr@gmail.com>
2025-08-30 19:09:00 +0900

    [merge] リザルトをマージ

--- Assets/Miya/Scripts/GameManager.cs
+++ Assets/Miya/Scripts/GameManager.cs
@@ -193,6 +193,7 @@ public class GameManager : MonoBehaviour
         if (ratios[^1] <= 0)
         {
             StopInkSpreading();
+            ResultManager.Instance.StartResult(CurrentStage, ratios);
         }
     }
 }
