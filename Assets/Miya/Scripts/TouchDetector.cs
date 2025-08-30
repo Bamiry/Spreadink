7302568c6a9cf2d991c150470974d3bdea47223a
FukaMiya <ninja8rrrr@gmail.com>
2025-08-30 18:10:28 +0900

    [add] SoundManagerの実装

--- Assets/Miya/Scripts/TouchDetector.cs
+++ Assets/Miya/Scripts/TouchDetector.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class TouchDetector : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     // PointerDown
